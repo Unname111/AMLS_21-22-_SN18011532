0c7a313ad0be6bd104df0eca3b01ba0e89d8530a
RoryZhangUK <94226473+RoryZhangUK@users.noreply.github.com>
2021-12-17 15:54:48 +0000

    Update and rename ASML_V0.1.py to PastVersion/ASML_V0.1.py

--- PastVersion/ASML_V0.1.py
+++ PastVersion/ASML_V0.1.py
@@ -4,7 +4,7 @@ This is the second trial of the code.
 
 The code cannot run properly today. Today mainly focus on the understanding of SVM and Kernel trick.
 
-Creator: Xuanchang Zhang
+
 Date: 28/11/2021
 """
 import cv2
